d1287a385115af92495edc228331dbba7f13af68
Juan Carlos Zepeda <juan97zepda@gmail.com>
2021-09-02 01:39:43 -0600

    start

---
+++ src/test/java/sv/com/ti/donationsite/DonationSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.ti.donationsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
