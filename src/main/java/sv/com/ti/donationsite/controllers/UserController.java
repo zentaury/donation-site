059cfda56e5d6de5f468ed2998262049421ea62a
Juan Carlos Zepeda <juan97zepda@gmail.com>
2021-09-02 19:56:10 -0600

    registro de usuarios terminado

--- src/main/java/sv/com/ti/donationsite/controllers/UserController.java
+++ src/main/java/sv/com/ti/donationsite/controllers/UserController.java
@@ -9,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sv.com.ti.donationsite.config.PasswordEncoder;
+import sv.com.ti.donationsite.domain.entities.CountryEntitie;
 import sv.com.ti.donationsite.domain.entities.RolEntitie;
 import sv.com.ti.donationsite.domain.entities.UserEntitie;
+import sv.com.ti.donationsite.domain.services.CountryService;
 import sv.com.ti.donationsite.domain.services.RolService;
 import sv.com.ti.donationsite.domain.services.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,6 +28,9 @@ public class UserController {
 
     @Autowired
     private RolService rolService;
+
+    @Autowired
+    private CountryService countryService;
 
     private PasswordEncoder passwordEncoder;
 
@@ -38,14 +44,19 @@ public class UserController {
     }
 
     @GetMapping("/user/registration")
-    public String registration(UserEntitie user){
+    public String registration(UserEntitie user, Model model){
+        List<CountryEntitie> countries = countryService.getAllCountries();
+        model.addAttribute("countries", countries);
         return "registration";
     }
 
     @PostMapping("/user/registration")
-    public String create(UserEntitie userEntitie){
+    public String create(UserEntitie userEntitie, Model model){
+
         userEntitie.setPassword(passwordEncoder.passwordEncypt(userEntitie.getPassword()));
+
         RolEntitie rol = new RolEntitie();
+
         userService.saveUser(userEntitie);
         rol.setUserId(userEntitie.getUserId());
         rol.setName("ROLE_USER");
